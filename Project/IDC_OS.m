4bc93838dae04af8163c8099b2ca4026f64a4610
Carl Larsson <130466691+Illuminy00@users.noreply.github.com>
2024-03-19 15:08:38 +0100

    compile error
    compile error

--- Project/IDC_OS.m
+++ Project/IDC_OS.m
@@ -4,12 +4,12 @@
 % Date: 18-03-2024
 %==============================================================================
 %% Clean up
-clc
 clear
 close all
 disp("==============================================================================")
 %% Define robot
 %------------------------------------------------------------------------------
+
 % Load 6R robot
 n_joints = 6;
 robot = loadrobot("universalUR5");
@@ -17,6 +17,7 @@ robot.DataFormat = 'column';
 robot.Gravity = [0,0,-9.81]';
 
 showdetails(robot)
+
 %------------------------------------------------------------------------------
 %% Matrices
 %------------------------------------------------------------------------------
@@ -34,47 +35,60 @@ K_D(5,5) = 3;
 K_D(6,6) = 3;
 
 %------------------------------------------------------------------------------
-%% Define initial and final
+%% Define positions (via points)
 % A = 0, B = 1, C = 2
 %------------------------------------------------------------------------------
 
+% Weights
+weights = [0.1, 0.1, 0.1, 1, 1, 1]';
+initial_guess = zeros(1, n_joints);
+
 % Initial, A
-x0 = transpose([0.25, 0.25, 0.25]);
+x0 = transpose([0.25, 0.25, 0.25, 0, 0, 0]);
 xd0 = zeros(n_joints,1);
 xdd0 = zeros(n_joints,1);
 t0 = 0;
+x0_pose = trvec2tform([x0(1), x0(2), x0(3)]) * eul2tform([x0(4), x0(5), x0(6)]);
 
 % B
-x1 = transpose([0.5, 0.5, 0.5]);
+x1 = transpose([0.5, 0.5, 0.5, 0, 0, 0]);
 xd1 = zeros(n_joints,1);
 xdd1 = zeros(n_joints,1);
 t1 = 5;
+x1_pose = trvec2tform([x1(1), x1(2), x1(3)]) * eul2tform([x1(4), x1(5), x1(6)]);
 
 % C
-x2 = transpose([0.4, 0.4, 0.4]);
+x2 = transpose([0.4, 0.4, 0.4, 0, 0, 0]);
 xd2 = zeros(n_joints,1);
 xdd2 = zeros(n_joints,1);
 t2 = 10;
+x2_pose = trvec2tform([x2(1), x2(2), x2(3)]) * eul2tform([x2(4), x2(5), x2(6)]);
 
 % Final, A
 t3 = 15;
+
 %------------------------------------------------------------------------------
-%% Visualize robot and trajectory
-%------------------------------------------------------------------------------
-% All via points, together forming entire trajectory A->B->C->A
-close all
-show(robot, q0)
-hold on
-show(robot, q1)
-show(robot, q2)
-show(robot, q0);
 %% Perform simulation in simulink
-out = sim('IDC_OS.slx');
-%% Redraw
+%------------------------------------------------------------------------------
+
 sampleTime = 0.001;
+out = sim('IDC_OS.slx');
+
+%------------------------------------------------------------------------------
+%% Create figure
+%------------------------------------------------------------------------------
+
+close all
+figure(); 
+
+%% Redraw
+
+sampleTime = 0.001;
+
 numSamples = size(out.q,3);
 jointsValueMat = reshape(out.q,[nJoints,numSamples]);
-showRobStates(robot,jointsValueMat,sampleTime);
+
 handtrace = true;
 showRobStates(robot,jointsValueMat,sampleTime,'handtrace',handtrace,'showedFPS',60,'PreservePlot',false);
+
 %------------------------------------------------------------------------------
